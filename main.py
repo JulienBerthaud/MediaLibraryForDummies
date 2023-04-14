fa607919e0df492591b5387d92219c92aa772859
JulienBerthaud <130763493+JulienBerthaud@users.noreply.github.com>
2023-04-14 15:19:50 +0000

    Commit Initial
    - Quelques commentaires dans le fichier principal.
    - Ajout  de la page d'accueil de la documentation.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
+# Déclarer le serveur web via Flask
 app = Flask(__name__)
 
-
+# 
+# Mapper l'appel du navigateur à l'url ../ à la réponse : Hello from Flask! 
+# 
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+  return 'Hello from Flask!'
 
-
+# Démarrer le erveur web
 app.run(host='0.0.0.0', port=81)
